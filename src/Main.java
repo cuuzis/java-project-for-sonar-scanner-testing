c3bd38b8aaaf0e816fdd6e8f1fd4e81201121f3e
Gustavs Venters <gustavs.venters@gmail.com>
2017-08-12 20:34:35 +0200

    Code smell long method: -1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        CauseLongMethod causeLongMethod = new CauseLongMethod();
         CauseClassDataShouldBePrivate1 causeClassDataShouldBePrivate1 = new CauseClassDataShouldBePrivate1();
         causeClassDataShouldBePrivate1.nonPrivate = 123;
         CauseClassDataShouldBePrivate2 causeClassDataShouldBePrivate2 = new CauseClassDataShouldBePrivate2();
